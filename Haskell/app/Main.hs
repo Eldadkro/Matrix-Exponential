89fa2fdf60b0690ccf7d4deebf29d52a27e6b469
Ruslan <2BC.Wasabi@gmail.com>
2023-01-13 00:30:18 +0200

    added scale for pade 13

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 import Prelude hiding((<>))
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
+phi13 = 5.371920351148152
 phiPairs = [(3,0.01495585217958292),(5,0.2539398330063230),(7,0.9504178996162932),(9,2.097847961257068)]
 bVector =  vector[1,182,16380,960960,40840800,1323241920,33522128640,670442572800,10559470521600,129060195264000,1187353796428800,7771770303897600,32382376266240000,64764752532480000]
 
@@ -29,9 +30,12 @@ calcPadeMU a t | a == 0 = multiplieMyMatrix a t
                | otherwise = scale (bVector ! fromIntegral a )  (multiplieMyMatrix a t) + calcPadeMU (a-1) t
 
 calcPadeM :: Integer -> Matrix Double -> (Matrix Double , Matrix Double)
-calcPadeM a t | a == -1 = (calcPadeMV (13 `div` 2) t, calcPadeMU (13 `div` 2) t)
+calcPadeM a t | a == -1 = (calcPadeMV (13 `div` 2) (scale s t), calcPadeMU (13 `div` 2) (scale s t))
               | otherwise =  (calcPadeMV (a `div` 2) t, calcPadeMU (a `div` 2) t)
-
+              where 
+                s = fromInteger intS
+                intS = floor logResult
+                logResult = logBase 2 ( norm_1 t / phi13)
 main :: IO ()
 main = do
     let a = (2><2)[ 1, 2, 3, 4 ] :: Matrix R
