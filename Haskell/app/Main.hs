b92095039f41adc5fec27d5bf956620e4bc34ea6
Ruslan <2BC.Wasabi@gmail.com>
2023-01-13 01:02:29 +0200

    added solve with the built in function
    need to check the answers to make sure that the other parts work correctly

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -21,27 +21,30 @@ multiplieMyMatrix a t | a == 0 = ident matrixSize
                       | a == 1 = t
                       | otherwise = t <> multiplieMyMatrix (a-1) t
 
-calcPadeMV :: Integer -> Matrix Double -> Matrix Double
-calcPadeMV a t | a == 0 = multiplieMyMatrix (a + 1) t
-               | otherwise = scale (bVector ! (fromIntegral a + 1))  (multiplieMyMatrix (a+1) t) + calcPadeMV (a-1) t
-
 calcPadeMU :: Integer -> Matrix Double -> Matrix Double
-calcPadeMU a t | a == 0 = multiplieMyMatrix a t
-               | otherwise = scale (bVector ! fromIntegral a )  (multiplieMyMatrix a t) + calcPadeMU (a-1) t
+calcPadeMU a t | a == 0 = multiplieMyMatrix (a + 1) t
+               | otherwise = scale (bVector ! (fromIntegral a + 1))  (multiplieMyMatrix (a+1) t) + calcPadeMU (a-1) t
+
+calcPadeMV :: Integer -> Matrix Double -> Matrix Double
+calcPadeMV a t | a == 0 = multiplieMyMatrix a t
+               | otherwise = scale (bVector ! fromIntegral a )  (multiplieMyMatrix a t) + calcPadeMV (a-1) t
 
 calcPadeM :: Integer -> Matrix Double -> (Matrix Double , Matrix Double)
-calcPadeM a t | a == -1 = (calcPadeMV (13 `div` 2) (scale s t), calcPadeMU (13 `div` 2) (scale s t))
-              | otherwise =  (calcPadeMV (a `div` 2) t, calcPadeMU (a `div` 2) t)
+calcPadeM a t | a == -1 = (calcPadeMU (13 `div` 2) (scale s t), calcPadeMV (13 `div` 2) (scale s t))
+              | otherwise =  (calcPadeMU (a `div` 2) t, calcPadeMV (a `div` 2) t)
               where 
                 s = fromInteger intS
                 intS = floor logResult
                 logResult = logBase 2 ( norm_1 t / phi13)
+
 main :: IO ()
 main = do
-    let a = (2><2)[ 1, 2, 3, 4 ] :: Matrix R
-    let b = (2><1)[ 5, 6 :: R]
-    mat <- loadMatrix "test.txt"
+
+    mat <- loadMatrix "ExpoMatrix1000x1000.txt"
     let ourM = checkNorm phiPairs mat
     let answer = calcPadeM ourM mat
-    let s = scale (bVector ! 0) (ident 5)
-    print s
+    print answer
+
+    let b = fst answer + snd answer
+    let a = snd answer - fst answer
+    print (linearSolve a b)
