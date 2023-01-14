c10ff4e61374ab0163ffab61c0a361ea13c50703
Ruslan <2BC.Wasabi@gmail.com>
2023-01-14 21:55:38 +0200

    rename

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -4,15 +4,13 @@ import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
 import Data.Maybe
 
-import Data.Time
-
-
 phi13 = 5.371920351148152
 phiPairs = [(3,0.01495585217958292),(5,0.2539398330063230),(7,0.9504178996162932),(9,2.097847961257068)]
 bVector =  vector[64764752532480000, 32382376266240000, 7771770303897600,1187353796428800, 129060195264000, 10559470521600,670442572800, 33522128640, 1323241920,40840800, 960960, 16380, 182, 1]
 
 matrixSize = 1000
 
+-- check matrix norm
 checkNorm :: [(Integer,Double)] -> Matrix Double -> Integer
 checkNorm [] _ = -1
 checkNorm [x] t | snd x > norm_1 t = fst x 
@@ -20,61 +18,67 @@ checkNorm [x] t | snd x > norm_1 t = fst x
 checkNorm (x:xs) t | snd x > norm_1 t = fst x 
                    | otherwise = checkNorm xs t
 
-
+-- mutiplie matrix can be optimized by having it ran from 0 to the int while passing the previous matrix forword
 multiplieMyMatrix :: Integer -> Matrix Double -> Matrix Double
-multiplieMyMatrix a t | a == 0 = ident matrixSize
-                      | a == 1 = t
-                      | otherwise = t <> multiplieMyMatrix (a-1) t
+multiplieMyMatrix times myMatrix | times == 0 = ident matrixSize
+                      | times == 1 = myMatrix
+                      | otherwise = myMatrix <> multiplieMyMatrix (times-1) myMatrix
 
+--calculate the U part of pade 
 calcPadeMU :: Integer -> Matrix Double -> Matrix Double
-calcPadeMU a t | a == 0 = scale b2p1 mat2p1
-               | otherwise = scale b2p1 mat2p1 + calcPadeMU (a-1) t
+calcPadeMU degree myMatrix | degree == 0 = scale b2p1 mat2p1
+               | otherwise = scale b2p1 mat2p1 + calcPadeMU (degree-1) myMatrix
                where 
-                  mat2p1 = multiplieMyMatrix k2p1 t
+                  mat2p1 = multiplieMyMatrix k2p1 myMatrix
                   b2p1 = bVector ! fromIntegral k2p1
-                  k2p1 = (2 * a) + 1
+                  k2p1 = (2 * degree) + 1
                   
-
+--calculate the V part of pade 
 calcPadeMV :: Integer -> Matrix Double -> Matrix Double
-calcPadeMV a t | a == 0 = scale b2 (multiplieMyMatrix 0 t)
-               | otherwise = scale b2 mat2 + calcPadeMV (a-1) t
+calcPadeMV degree myMatrix | degree == 0 = scale b2 (multiplieMyMatrix 0 myMatrix)
+               | otherwise = scale b2 mat2 + calcPadeMV (degree-1) myMatrix
                 where  
-                  mat2 = multiplieMyMatrix k2 t
+                  mat2 = multiplieMyMatrix k2 myMatrix
                   b2 = bVector ! fromIntegral k2
-                  k2 = 2 * a
+                  k2 = 2 * degree
 
+-- calculate pade of degree M returns a pair of matrices (U,V)
 calcPadeM :: Integer -> Integer ->  Matrix Double -> (Matrix Double , Matrix Double)
-calcPadeM a s t | a == -1 = (calcPadeMU (13 `div` 2) (scale toScale t), calcPadeMV (13 `div` 2) (scale toScale t))
-              | otherwise =  (calcPadeMU (a `div` 2) t, calcPadeMV (a `div` 2) t)
+calcPadeM degree s myMatrix | degree == -1 = (calcPadeMU pade13Index scaledMatrix, calcPadeMV pade13Index scaledMatrix)
+              | otherwise =  (calcPadeMU padeIndex myMatrix, calcPadeMV padeIndex myMatrix)
               where 
                 toScale = 1 / 2^s
+                padeIndex = degree `div` 2
+                pade13Index = 13 `div` 2
+                scaledMatrix = scale toScale myMatrix
 
+-- calculate the scaling factor 
 getS :: Matrix Double -> Integer
-getS t = s
+getS myMatrix = s
         where 
         s = ceiling logResult
-        logResult = logBase 2 ( norm_1 t / phi13)
+        logResult = logBase 2 ( norm_1 myMatrix / phi13)
 
+-- scale back function
 scaleBack :: Integer -> Matrix Double -> Matrix Double
-scaleBack s amatrix | s == 0 = amatrix
+scaleBack s myMatrix | s == 0 = myMatrix
               | otherwise = sm1<>sm1
               where
-                sm1 = scaleBack (s-1) amatrix
+                sm1 = scaleBack (s-1) myMatrix
 
-shouldScaleBack m s amatrix | m == -1 = scaleBack s amatrix
-                            | otherwise =  amatrix
+shouldScaleBack m s myMatrix | m == -1 = scaleBack s myMatrix
+                            | otherwise =  myMatrix
 
-
-
-
-kaczmarzLoopNumber = 1000
-totalLoops = 100
-runKaczmarzLoop :: Matrix Double -> Matrix Double -> Matrix Double -> Int -> Matrix Double
-runKaczmarzLoop a b x i | i >= totalLoops = x
-                          | otherwise = runKaczmarzLoop a b newX (i+1)
+numberOfSweeps = 100
+-- run kaczmarz sweep
+runKaczmarzSweeps :: Matrix Double -> Matrix Double -> Matrix Double -> Int -> Matrix Double
+runKaczmarzSweeps a b x i | i >= numberOfSweeps = x
+                          | otherwise = runKaczmarzSweeps a b newX (i+1)
                           where
                               newX = fromColumns (kaczmarzForEach a b x matrixSize 0)
 
+kaczmarzLoopNumber = 1000
+--for each column of x perform kaczmarz
 kaczmarzForEach :: Matrix Double -> Matrix Double -> Matrix Double -> Int -> Int -> [Vector Double]
 kaczmarzForEach a b x n i | i >= n = []
                           | otherwise = kacI  : kaczmarzForEach a b x n (i+1)
@@ -82,19 +86,7 @@ kaczmarzForEach a b x n i | i >= n = []
                               kacI = kaczmarz' a bi xi kaczmarzLoopNumber 0
                               xi = flatten (x ¿ [i])
                               bi = flatten (b ¿ [i])
-                              
-
-
-
-changeV2L :: Int -> [Vector Double] -> [[Double]]
-changeV2L index x | index < (matrixSize - 1) = myloop
-                  | otherwise = [toList (x!!index)]
-                  where 
-                    myloop = toList (x!!index) : changeV2L (index+1) x
-
-kaczmarz :: Matrix Double -> Vector Double -> Vector Double -> Int -> Vector Double
-kaczmarz a b x n = kaczmarz' a b x n 0
-
+-- perform kaczmarz algorithm                               
 kaczmarz' :: Matrix Double -> Vector Double -> Vector Double -> Int -> Int -> Vector Double
 kaczmarz' a b x n i
     | i >= n = x
@@ -118,7 +110,7 @@ padeApproxemteLS aMatrix = finalAnswer
                           n =  1000 :: Int 
                           zero = 0 :: Double
                           x = konst zero (n,n)
-                          kaczmarzAnswer = runKaczmarzLoop q p x 0
+                          kaczmarzAnswer = runKaczmarzSweeps q p x 0
                           finalAnswer = shouldScaleBack ourM ourS kaczmarzAnswer
 
 
