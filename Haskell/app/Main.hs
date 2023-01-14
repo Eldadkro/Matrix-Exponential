d02d79885e3eeea63f03dec34c32250436ef57f0
Ruslan <2BC.Wasabi@gmail.com>
2023-01-14 02:36:02 +0200

    kaczmarz

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 import Prelude hiding((<>))
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
+import GHC.Plugins (lookupPackageName)
 phi13 = 5.371920351148152
 phiPairs = [(3,0.01495585217958292),(5,0.2539398330063230),(7,0.9504178996162932),(9,2.097847961257068)]
 bVector =  vector[64764752532480000, 32382376266240000, 7771770303897600,1187353796428800, 129060195264000, 10559470521600,670442572800, 33522128640, 1323241920,40840800, 960960, 16380, 182, 1]
@@ -56,6 +57,45 @@ shouldScaleBack a s t | a == -1 = t^toScale
               where 
                 toScale = 2^s 
 
+
+kaczmarzLoopNumber = 50
+
+kaczmarzFunc :: Int -> [Vector Double] -> Vector Double -> Vector Double -> [Vector Double]
+kaczmarzFunc index rowsA colB xj | index == matrixSize = [xj]
+                                 | otherwise = kaczmarzFunc (index + 1) rowsA colB xjp1
+                                    where 
+                                      xjp1 = xj + flatten proj
+                                      proj = scale ((bi - dotAiXjp1)/lenAi) aColVector
+                                      bi = colB ! index
+                                      dotAiXjp1 = (aRowVector * asColumn xj) `atIndex` (0,0)
+                                      lenAi = (aRowVector * aColVector) `atIndex` (0,0)
+                                      aColVector = asColumn (rowsA!!index)
+                                      aRowVector = asRow (rowsA!!index)
+
+kaczmarzForEach :: Int -> [Vector Double] -> [Vector Double] -> [Vector Double] -> [Vector Double]
+kaczmarzForEach index rows columns x | index < matrixSize = myloop
+                                     | otherwise = x
+                                      where 
+                                        myloop = xj ++ xRest
+                                        xj = kaczmarzFunc 0 rows (columns!!index) (x!!index)
+                                        xRest = kaczmarzForEach (index+1) rows columns x
+
+                              
+
+--kaczmarzForEach index rows = rows ++ kaczmarzFunc index
+
+kaczmarzLoop :: Int -> [Vector Double] -> [Vector Double] -> [Vector Double] -> [Vector Double]
+kaczmarzLoop index rowsA columnsB x | index == kaczmarzLoopNumber = x
+                                    | otherwise = next 
+                                    where 
+                                      newX = kaczmarzForEach 0 rowsA columnsB x
+                                      next = kaczmarzLoop (index+1) rowsA columnsB newX
+
+changeV2L :: Int -> [Vector Double] -> [[Double]]
+changeV2L index x | index < (matrixSize - 1) = myloop
+                  | otherwise = [toList (x!!index)]
+                  where 
+                    myloop = toList (x!!index) : changeV2L (index+1) x
 main :: IO ()
 main = do
 
@@ -65,14 +105,31 @@ main = do
     let answer = calcPadeM ourM ourS mat
     let u = fst answer
     let v = snd answer
-    saveMatrix "haskell-Utest.txt" "%0.10f" u
-    saveMatrix "haskell-Vtest.txt" "%0.10f" v
     let p = u + v
     let q = v - u
-    saveMatrix "haskell-Qtest.txt" "%0.10f" q
-    saveMatrix "haskell-Ptest.txt" "%0.10f" p
+
+    --Kaczmarz method
+    let rowsA = toRows q
+    let columnsB = toColumns p
+    randx <- randn 1000 1000
+    let columnsX = toColumns randx
+    let listXCols = kaczmarzLoop 0 rowsA columnsB columnsX
+    print "aaa"
+    writeFile "file2.txt" (show (changeV2L 0 listXCols))
+    print "aaa"
+    --writeFile "file.txt" (show listXCols)
+    
+    print "aaa"
+    --let ourSol = fromRows listXCols
+    --print (rows ourSol)
+    --print (cols ourSol)
+    print "bbb1"
+    --saveMatrix "OurSolve" " %f ," ourSol
+    print "bbb"
+    --let ourSolfinalAnswer = shouldScaleBack ourM ourS ourSol
+    --saveMatrix "OurSolve" " %f ," ourSolfinalAnswer
+
     let answer2 = linearSolveLS q p 
-    saveMatrix "beforeFinalScale.txt" " %f ," answer2
 
     --print answer2
     let finalAnswer = shouldScaleBack ourM ourS answer2
